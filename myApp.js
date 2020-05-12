f6d5792e0e60a4f7d05c829901db39a7dbb8151c
Esteban Camargo <estebmaister@gmail.com>
2020-05-12 18:08:37 -0400

    :zap: Make the necesary requests

--- myApp.js
+++ myApp.js
@@ -14,7 +14,7 @@
 //
 // mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true });
 require("dotenv").config();
-require("mongoose");
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
