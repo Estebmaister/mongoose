11bdc8a8f503826a637d133223e7a6c4d21e8ebc
Esteban Camargo <estebmaister@gmail.com>
2020-05-18 02:14:34 -0400

    :card_file_box: Added find person by id function

--- myApp.js
+++ myApp.js
@@ -186,8 +186,11 @@ const findOneByFood = function (food, done) {
 // using `Model.findById() -> Person`.
 // Use the function argument 'personId' as search key.
 
-var findPersonById = function (personId, done) {
-  done(null /*, data*/);
+const findPersonById = function (personId, done) {
+  Person.findById(personId, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 /** # CR[U]D part III - UPDATE # 
